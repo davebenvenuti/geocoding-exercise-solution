f352ae819296db35101340bb08faaa24201619dc
Dave Benvenuti <davebenvenuti@gmail.com>
2016-10-11 18:26:23 -0400

    initial commit of express app, hasn't been tested yet

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var geohash = require('./geohash');
+
+app.get('/geohash', function (req, res) {
+    geohash.generate(req.params.lat, req.params.lon)
+        .then(function(results) {
+            res.json({
+                lat: results[0],
+                lon: results[1]
+            });
+        })
+        .catch(err) {
+            res.status(500).json(err);
+        };
+
+});
+
+var port = process.env.PORT || 3000;
+
+if(process.env.TEST == 'true') {
+    module.exports.app = app;
+} else {
+    app.listen(port, function () {
+        console.log('Example app listening on port ' + port + '!');
+    });
+}
---
+++ app_test.js
@@ -0,0 +1,7 @@
+var request = require('supertest');
+
+describe('app', function() {
+    it('returns a geohash for a given lat/lon', function(done) {
+
+    });
+});
