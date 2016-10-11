bada6aa3b73efbffe08a4e48c8840f1b4ba9dcdb
Dave Benvenuti <davebenvenuti@gmail.com>
2016-10-11 18:26:10 -0400

    dow fetcher now accepts specified dates

--- dow.js
+++ dow.js
@@ -1,21 +1,54 @@
-var quoteURL = "http://real-chart.finance.yahoo.com/table.csv?s=^DJI"
+
+// http://chart.finance.yahoo.com/table.csv?s=^DJI&a=9&b=11&c=2015&d=9&e=11&f=2016&g=d&ignore=.csv
 
 var request = require('request');
-var Promise = require('bluebird');
 var parseCSV = require('csv-parse');
+var promiseOrCallback = require('./util').promiseOrCallback;
+var moment = require('moment');
 
-module.exports.latestOpen = function() {
-    var callback = (typeof arguments[0] == 'function') ? arguments[0] : null;
+var quoteURL = function(date) {
+    // URL obtained from https://finance.yahoo.com/quote/%5EDJI/history, download data link
+    var base = "http://real-chart.finance.yahoo.com/table.csv?s=^DJI";
 
-    var fetch = function(callback, resolve, reject) {
-        request.get({ url: quoteURL }, function(err, res, body) {
+    if(date) {
+        date = moment(date); // make sure we're dealing with a moment object
+
+        // the yahoo finance url appears to accept the start and end date in
+        // the form:
+        //
+        //   a=[start month 0 indexed]&b=[start date 1 indexed]&c=[start year]
+        //   &d=[end month 0 index]&e=[end date 1 indexed]&f=[end year]
+        //
+        // make the end date our requested date, then go back 7 days, to ensure
+        // the top row of the csv is either the date we requested or the most
+        // recent date with a quote
+
+        var sevenDaysAgo = moment(date).subtract(7, 'days');
+
+        return base +
+            "&a=" + sevenDaysAgo.month() +
+            "&b=" + sevenDaysAgo.date() +
+            "&c=" + sevenDaysAgo.year() +
+            "&d=" + date.month() +
+            "&e=" + date.date() +
+            "&f=" + date.year()
+
+    } else {
+        return base;
+    }
+};
+
+module.exports.latestOpen = function(date, callback) {
+    if(typeof date == 'function') {
+        callback = date;
+        date = null;
+    }
+
+    return promiseOrCallback(callback, function(resolve, reject) {
+        request.get({ url: quoteURL(date) }, function(err, res, body) {
 
             if(err) {
-                if(callback) {
-                    callback(err);
-                } else {
-                    reject(err);
-                }
+                reject(err);
             } else {
 
                 parseCSV(body, {}, function(err, parsed) {
@@ -24,23 +57,9 @@ module.exports.latestOpen = function() {
                     //
                     // The format of the csv is:
                     //   Date, Open, High, Low, Close, Volume, Adj Volume
-                    if(callback) {
-                        callback(null, parsed[1] && parsed[1][1]);
-                    } else {
-                        resolve(parsed[1] && parsed[1][1]);
-                    }
+                    resolve(parsed[1] && parseFloat(parsed[1][1]).toFixed(2));
                 });
-
-
             }
         });
-    };
-
-    if(callback) {
-        fetch(callback);
-    } else {
-        return new Promise(function(resolve, reject) {
-            fetch(null, resolve, reject);
-        });
-    }
+    });
 };
--- test/unit/dow_test.js
+++ test/unit/dow_test.js
@@ -1,12 +1,13 @@
 var expect = require('expect.js');
 
 var dow = require('../../dow');
-var replay = require('replay');
+
+require('replay');
 
 describe('dow', function() {
     it('fetches the latest dow jones open price and passes to a callback', function(done) {
         dow.latestOpen(function(err, openPrice) {
-            expect(openPrice).to.eql(18282.949219);
+            expect(openPrice).to.eql(18282.95);
 
             done();
         });
@@ -15,9 +16,18 @@ describe('dow', function() {
     it('fetches the latest dow jones open price and returns a promise', function(done) {
         dow.latestOpen()
             .then(function(openPrice) {
-                expect(openPrice).to.eql(18282.949219);
+                expect(openPrice).to.eql(18282.95);
 
                 done();
             });
     });
+
+    it('fetches the dow jones open price for a specific date and returns a promise', function(done) {
+        dow.latestOpen('2005-05-26')
+            .then(function(openPrice) {
+                expect(openPrice).to.eql(10458.68);
+
+                done();
+            });
+    });    
 });
